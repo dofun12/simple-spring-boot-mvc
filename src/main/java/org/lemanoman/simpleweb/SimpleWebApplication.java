c8858c96ed9e2f96ab7a6cb98b010972b22c8362
kevim <k.v.n.such@gmail.com>
2019-03-21 14:45:12 -0300

    Arrumando probleminhas de dependencia

--- src/main/java/org/lemanoman/simpleweb/SimpleWebApplication.java
+++ src/main/java/org/lemanoman/simpleweb/SimpleWebApplication.java
@@ -3,16 +3,11 @@ package org.lemanoman.simpleweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.ExecutorService;
-
 @SpringBootApplication
 public class SimpleWebApplication {
 
-	public static void main(String[] args) {
-		Thread thread = new Thread(new TesteRunnable());
-		thread.start();
-
-		SpringApplication.run(SimpleWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplication.class, args);
+    }
 
 }
